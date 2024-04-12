9b34f5932c2a2a5ab55482490a063da908fad403
Md Faizy <mdgmfaizy@gmail.com>
2024-04-12 11:10:15 +0530

    update

--- index.js
+++ index.js
@@ -1,47 +1,72 @@
-// const express =  require('express');
-// const app = express();
+// const express = require('express');
 // const bodyParser = require('body-parser');
 // const cookieParser = require('cookie-parser');
 // const fileUpload = require('express-fileupload');
 // const cors = require("cors");
+// const dotenv = require("dotenv");
+// const app = express();
 
-// require("dotenv").config();
+// // Load environment variables from .env file
+// // app.use(cors());
+// app.use(cors({
+//   origin: 'http://127.0.0.1:5173'
+// }));
+
+
+
+// dotenv.config();
+// require("./config/database").connect();
+// // Middleware setup
 // app.use(cookieParser());
-// app.use(cors());
 
-
-// const userRoutes = require("./routes/user");
-// const userProfile=require("./routes/profile")
 // app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true })); 
-// app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// require("./config/database").connect()
+// // File upload middleware setup
+// app.use(fileUpload({
+//   useTempFiles: true,
+//   tempFileDir: "/tmp/",
+// }));
 
+// // Database connection
+
+
+// // Importing routes
+// const userRoutes = require("./routes/user");
+// const userProfile = require("./routes/profile");
+
+// // Routes setup
 // app.use("/api/v1/auth", userRoutes);
-// app.use("/api/v1/profile",userProfile);
+// app.use("/api/v1/profile", userProfile);
+
+// // Cloudinary connection
+// const cloudinary = require("./config/cloudinary.js");
+// cloudinary.cloudinaryConnect();
 
 // const PORT = process.env.PORT || 3000;
 
+// // Start server
+// app.listen(PORT, () => {
+//   console.log("Server is running on port", PORT);
+// });
 
-// app.use(
-//     fileUpload({
-//       useTempFiles: true,
-//       tempFileDir: "/tmp/",
-//     })
-//   );
-//   require("./config/database").connect();
+// // Root route
+// app.get("/", (req, res) => {
+//   res.send("<h1>Auth App</h1>");
+// });
 
-// //cloud se connect
-// const cloudinary = require("./config/cloudinary.js");
-// cloudinary.cloudinaryConnect();
-// app.listen(PORT,() => {
-//     console.log("Server Run at ",PORT);
-// })
 
-// app.get("/", (req,res) => {
-//     res.send("<h1>Auth App</h1>")
-// })
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -58,16 +83,14 @@ const dotenv = require("dotenv");
 const app = express();
 
 // Load environment variables from .env file
-// app.use(cors());
+dotenv.config();
+
+// Middleware setup
 app.use(cors({
   origin: 'http://127.0.0.1:5173'
 }));
-dotenv.config();
 
-require("./config/database").connect();
-// Middleware setup
 app.use(cookieParser());
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -78,7 +101,7 @@ app.use(fileUpload({
 }));
 
 // Database connection
-
+require("./config/database").connect();
 
 // Importing routes
 const userRoutes = require("./routes/user");
