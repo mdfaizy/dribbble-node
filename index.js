9d098fc1beae39837ae54aef953d0ea5ca03c3bc
Md Faizy <mdgmfaizy@gmail.com>
2024-04-13 10:44:51 +0530

    update

--- index.js
+++ index.js
@@ -90,10 +90,7 @@ app.use(cors({
   origin: 'http://127.0.0.1:5173',
   credentials: true,
 }));
-// app.use(cors({
-//   origin: 'http://127.0.0.1:5173',
-//   credentials: true,
-// }));
+
 
 app.use(cookieParser());
 app.use(bodyParser.json());
