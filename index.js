f681c114b419d1e65d86f3e20480b7df18be8aae
Md Faizy <mdgmfaizy@gmail.com>
2024-04-15 10:58:51 +0530

    update

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ const app = express();
 dotenv.config();
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    
+    origin:"*",
+    optionSuccessstatus:200,
+    
     credentials: true,
   })
 );
